e65fa8b60a70a6636ed6831a079674b649f5a045
97gushan <97gustavh@gmail.com>
2016-01-28 16:47:54 +0100

    created a physics file and a gravity function
    This file is going to hold all of the physical calculations.
    
    The gravity function takes the velocity and delta_time as parameters. It
    then calculates the new velocity after it has accelerated. The distance
    traveled is then calculated and distance and velocity is returned

---
+++ physics.py
@@ -0,0 +1,17 @@
+
+def gravity(v, dt):
+    """ this method simulates the fall of an object with
+        the acceleration of 9.82"""
+        
+    # a constant
+    k = 0.01
+    
+    # calculate the new velocity
+    v = v + (k + dt) * 9.82
+    
+    # calculate the distance traveled
+    dy = (k + dt) * v
+    
+    # return the distance and the velocity
+    return(dy, v)
+    
