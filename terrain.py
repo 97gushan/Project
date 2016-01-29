8e503303ebfe2c0af94494d150c0a1cd8578d8e3
97gushan <97gustavh@gmail.com>
2016-01-29 21:56:31 +0100

    superclass terrain and subclasses ground and wall

--- terrain.py
+++ terrain.py
@@ -1,9 +1,21 @@
-
-class Ground:
-    """ this class describes how the ground will work"""
-    def __init__(self, xpos, ypos, width):
+class Terrain:
+    def __init__(self, xpos, ypos):
         self._xpos = xpos
         self._ypos = ypos
+        
+    def get_x(self):
+        return self._xpos
+    
+    def get_y(self):
+        return self._ypos
+
+
+class Ground (Terrain):
+    """ this class describes how the ground will work"""
+    def __init__(self, xpos, ypos, width):
+        
+        super().__init__(xpos, ypos)
+
         self._width = width
         
     def draw(self, pg, window):
@@ -15,41 +27,25 @@ class Ground:
         height = 800 - self._ypos
         rect = pg.Rect(self._xpos, self._ypos, self._width,height)
         
-
-
-        
         pg.draw.rect(window,box_color,rect,0)
         pg.draw.line(window, line_color, (self._xpos, self._ypos), (self._xpos + self._width, self._ypos),1)
         
-        
-    def get_x(self):
-        return self._xpos
-    
-    def get_y(self):
-        return self._ypos
         
     def get_width(self):
         return self._width
         
 
-class Wall:
+class Wall(Terrain):
     """ this class describes how the walls will work"""
     
     def __init__(self, xpos, ypos, height, direc):
-        self._xpos = xpos
-        self._ypos = ypos
+        super().__init__(xpos, ypos)
         self._height = height
         self._direc = direc
         
     def draw(self, pg, window):
         pg.draw.line(window, (0,255,255), (self._xpos, self._ypos), (self._xpos, self._ypos + self._height),1)
         
-        
-    def get_x(self):
-        return self._xpos
-    
-    def get_y(self):
-        return self._ypos
         
     def get_height(self):
         return self._height
