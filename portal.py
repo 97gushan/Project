06009b2178fe50125634d22a3ce171f0f87ec0f1
97gushan <97gustavh@gmail.com>
2016-01-29 22:10:30 +0100

    added the attribute active,made get & set methods

--- portal.py
+++ portal.py
@@ -6,12 +6,14 @@ class Portal:
         self._color = color
         self._angle = angle
     
+        self._active = False
     
     def draw(self, pg, window):
         """ draw the portal"""
-        rect = pg.Rect(self._xpos, self._ypos, 30,50)
-        
-        pg.draw.rect(window,self._color,rect,0)
+        if(self._active):
+            rect = pg.Rect(self._xpos, self._ypos, 30,50)
+            
+            pg.draw.rect(window,self._color,rect,0)
     
     
     # get  and set methods
@@ -23,6 +25,9 @@ class Portal:
 
     def get_angle(self):
         return self._angle
+        
+    def get_active(self):
+        return self._active
 
     def set_x(self, xpos):
         self._xpos = xpos
@@ -32,3 +37,6 @@ class Portal:
     
     def set_angle(self, angle):
         self._angle = angle
+        
+    def set_active(self, state):
+        self._active = state
